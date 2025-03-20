8b139447c2ca54e1a8a9b60e48e16d6c7b3cf6b6
Your Name <you@example.com>
2025-03-20 18:41:37 +0100

    powerUp

--- mob.gd
+++ mob.gd
@@ -16,11 +16,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	#Recorrer getConnections y desconectar.
 
 func has_xocat() -> void:
-	print_debug("xoco")
 	var power_up=power_upScene.instantiate() as PowerUp
-	print_debug("instancio powerUp")
 	power_up.load_from(powerUpConfigurations[randi_range(0, powerUpConfigurations.size()-1)])
 	power_up.position=global_position
-	print_debug(power_up.global_position)
 	onXocarMob.emit(power_up)
 	queue_free()
